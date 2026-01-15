4dbe5ab86c09989d825896027dbb389ef0058f7a
Naveen Dali <dalinaveen1432@gmail.com>
2026-01-15 22:01:39 +0530

    modified add function

--- add_function.py
+++ add_function.py
@@ -1,1 +1,9 @@
-print('this is add function.')
+def add(n1,n2):
+    c=n1+n2
+    return c
+
+if __name__=='__main__':
+    n1=int(input('Enter first value :'))
+    n2=int(input("Enter second value :"))
+    result = add(n1,n2)
+    print('sum =',result)
